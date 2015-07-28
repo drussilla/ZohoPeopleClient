55dee3cdbc907e7dd4dab376e3869c0ca1505afd
Ivan Derevianko <drussilla7@gmail.com>
2015-07-28 23:23:42 +0200

    FileDataAttribute added to unit test framework

---
+++ ZohoPeopleClient.Test/FileDataAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using Xunit.Sdk;
+
+namespace ZohoPeopleClient.Test
+{
+    public class FileDataAttribute : DataAttribute
+    {
+        public override IEnumerable<object[]> GetData(MethodInfo testMethod)
+        {
+            var dataFilePath = testMethod.Name + ".json";
+            yield return new object[] { File.ReadAllText(dataFilePath) };
+        }
+    }
+}
